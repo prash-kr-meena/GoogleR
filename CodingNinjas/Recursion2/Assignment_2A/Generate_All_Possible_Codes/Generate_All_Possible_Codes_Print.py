23f4d8e396b8c7dae2b8248c748d16c445defb8d
prash-kr-meena <social.prash@gmail.com>
2020-12-29 19:30:59 +0530

    Generate_All_Possible_Codes - Print

---
+++ CodingNinjas/Recursion2/Assignment_2A/Generate_All_Possible_Codes/Generate_All_Possible_Codes_Print.py
@@ -0,0 +1,12 @@
+"""
+You can do, its implementation for Index or Length based
+
+Both the implementations, would look very similar to that of the
+Generate_All_Possible_Codes_Return__Idx_Impl.py  and
+Generate_All_Possible_Codes_Return__Len_Impl.py
+
+The only difference, will be
+1. We don't require to pass a list, to store the codes. (which we are adding in the base case)
+2. Instead of adding the code, into the list just directly print it
+3. We don't need to return the list, so remove the return statement
+"""
