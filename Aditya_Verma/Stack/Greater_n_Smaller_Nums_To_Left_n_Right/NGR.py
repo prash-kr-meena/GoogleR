e47ea0a37da251213d2796d6c547ea4a80b522b7
prash-kr-meena <social.prash@gmail.com>
2021-01-04 16:27:46 +0530

    NGR     |     Nearest Greater to right     |    Next Largest Element (GfG)

---
+++ Aditya_Verma/Stack/Greater_n_Smaller_Nums_To_Left_n_Right/NGR.py
@@ -0,0 +1,72 @@
+from typing import List
+
+"""
+NGR     |     Nearest Greater to right     |     Next Largest Element (GfG)
+"""
+
+
+# Brute Force Solution
+# O(n2) Time
+# O(1) Space
+def ngr_brute_force(nums) -> List[int]:
+    print("Brute Force Solution : ")
+    length = len(nums)
+    result = [-1] * len(nums)  # Initializing all with -1 already
+
+    for i in range(0, length):
+        for j in range(i + 1, length):  # j depends on i
+            if nums[j] > nums[i]:  # First element that we found greater, is our next greater
+                result[i] = nums[j]
+                break  # Important, as we don't want to look more, once we find first element
+    print(nums)
+    print(result)
+    return result
+
+
+"""
+using list as stack
+pop : list.pop()            remove element from end
+push : list.append()        add element from end
+top : list[-1]          get last element
+size : len(list)            get the size
+"""
+
+
+def next_greatest_element_to_right(nums):
+    print("Optimized Stack Solution : ")
+    length = len(nums)
+    result = [-1] * len(nums)  # Initializing all with -1 already
+
+    stack = []  # Using list as stack
+
+    for i in range(length - 1, -1, -1):  # n-1 to 0 in reverse
+        curr = nums[i]
+
+        if len(stack) == 0:
+            result[i] = curr  # pushing -1
+
+        elif len(stack) != 0 and stack[-1] > curr:  # stack_top > curr element
+            result[i] = stack[-1]  # stack_top
+
+        elif len(stack) != 0:  # stack_top <= curr element
+            while len(stack) != 0 and stack[-1] > curr:
+                stack.pop()
+
+            # By which of the above condition, the loop has ended
+            if len(stack) == 0:
+                result[i] = -1  # pushing -1                similar to our 1st condition
+            else:  # found an element greater then curr_element
+                result[i] = stack[-1]  # stack_top          similar to our 2nd condition
+
+        # Handled the current element
+        stack.append(curr)
+
+    print(nums)
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().strip().split()))
+    ngr_brute_force(arr)
+    next_greatest_element_to_right(arr)
