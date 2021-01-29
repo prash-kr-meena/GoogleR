665ee070d4a439e037881222aee5f59411f6d4ee
prash-kr-meena <social.prash@gmail.com>
2021-01-29 15:45:46 +0530

    Maximum_Area_Rectangle_In_Binary_Matrix

--- Aditya_Verma/Stack/Maximum_Area_Rectangle_In_Binary_Matrix.py
+++ Aditya_Verma/Stack/Maximum_Area_Rectangle_In_Binary_Matrix.py
@@ -1,6 +1,11 @@
 from Aditya_Verma.Stack.Maximum_Area_Histogram import mah
 from Utils.Matrix import input_integer_matrix
 
+"""
+Time  : O(n * m)
+Space : O(n * m)
+"""
+
 
 def create_building_for_this_level(rolling_buildings, buildings):
     # Notice : modifying rolling_buildings
@@ -11,14 +16,17 @@ def create_building_for_this_level(rolling_buildings, buildings):
             rolling_buildings[index] = rolling_buildings[index] + building_height
 
 
-def find_max_area_reactangle_in_binary_matrix(matrix):
-    row_size = len(matrix)
-    rolling_buildings = [0] * row_size  # Notice : this is a rolling array
+def find_max_area_rectangle_in_binary_matrix(matrix):
+    if len(matrix) == 0:  # Edge Case
+        return 0
+
+    columns = len(matrix[0])  # considering column size would be fixed, Ie No Jagged Array
+    rolling_buildings = [0] * columns  # Notice : this is a rolling array
 
     max_area = float("-inf")
     for buildings in matrix:
         create_building_for_this_level(rolling_buildings, buildings)  # updating rolling_buildings
-        max_area_for_these_building = mah(rolling_buildings)
+        max_area_for_these_building = mah(rolling_buildings)  # Notice : using maximum_area_histogram
         max_area = max(max_area_for_these_building, max_area)
 
     return max_area
@@ -26,7 +34,7 @@ def find_max_area_reactangle_in_binary_matrix(matrix):
 
 if __name__ == '__main__':
     arr2D = input_integer_matrix()
-    max_area = find_max_area_reactangle_in_binary_matrix(arr2D)
+    max_area = find_max_area_rectangle_in_binary_matrix(arr2D)
     print(max_area)
 
 """
@@ -36,4 +44,20 @@ col : 4
 1 1 1 1
 1 1 1 1
 1 1 0 0
+
+
+row : 3
+col : 3
+0 1 1
+1 1 1
+0 1 1
+
+
+row : 4
+col : 5
+1 0 1 0 0 
+1 0 1 1 1 
+1 1 1 1 1 
+1 0 0 1 0
+
 """
