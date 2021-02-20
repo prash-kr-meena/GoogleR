2fe002803e1c83d44e776b96ff6919dcb495cd92
prashant meena <prashant.meena@olacabs.com>
2021-02-20 20:33:03 +0530

    Sum_Of_Nodes

---
+++ Tree/CodingNinjas/1GenericTree/Sum_Of_Nodes.py
@@ -0,0 +1,24 @@
+from GenericTree import GenericTree
+
+
+def find_sum_of_all_nodes(root):
+    if root is None:
+        return 0  # no nodes, so sum is 0
+
+    subtree_sum = 0
+    for child in root.children:
+        subtree_sum += find_sum_of_all_nodes(child)
+
+    total_sum = subtree_sum + root.data
+    return total_sum
+
+
+if __name__ == '__main__':
+    root = GenericTree.input_one_line()
+    # GenericTree.print_level_order(root)
+    print(find_sum_of_all_nodes(root))
+
+"""
+10 3 20 30 40 2 40 50 0 0 0 0
+190
+"""
