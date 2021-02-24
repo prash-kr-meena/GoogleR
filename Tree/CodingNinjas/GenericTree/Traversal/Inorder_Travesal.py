d4ff180ae16728949b377ca143d369774eccef45
prash-kr-meena <social.prash@gmail.com>
2021-02-24 14:35:41 +0530

    preorder traversal

--- Tree/CodingNinjas/GenericTree/Traversal/Inorder_Travesal.py
+++ Tree/CodingNinjas/GenericTree/Traversal/Inorder_Travesal.py
@@ -1,3 +1,4 @@
 """
-There is no inorder for a generic Tree
+There is no inorder for a generic Tree, as such
+But on GFG they have defined one possible sequence, which  tbh is not that important
 """
