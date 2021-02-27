978eb34eeb2a8bf2900965ab68f9d552971ff700
prash-kr-meena <social.prash@gmail.com>
2021-02-27 15:32:25 +0530

    left and Right view

---
+++ Tree/BinaryTree/Tree_View/Bottom_View_Of_Binary_Tree.py
@@ -0,0 +1,12 @@
+from Tree.BinaryTree.BinaryTree import BinaryTree
+from Utils.Array import input_array
+
+
+def bottom_view_of_binary_tree(root):
+    pass
+
+
+if __name__ == '__main__':
+    tree_root = BinaryTree.single_line_input(input_array(""))
+    BinaryTree.display(tree_root)
+    bottom_view_of_binary_tree(tree_root)
---
+++ Tree/BinaryTree/Tree_View/Left_View_Of_Binary_Tree.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+from Tree.BinaryTree.BinaryTree import BinaryTree
+from Utils.Array import input_array
+
+
+def left_view_of_binary_tree(root):
+    queue = deque()
+    queue.append(root)
+
+    while len(queue) != 0:
+        size = len(queue)
+        pointer = 0  # start index with 0
+        while pointer != size:
+            curr_root = queue.popleft()
+            if pointer == 0:  # print first element
+                print(curr_root.data, end=" ")
+
+            if curr_root.left is not None:
+                queue.append(curr_root.left)
+
+            if curr_root.right is not None:
+                queue.append(curr_root.right)
+
+            pointer += 1
+        # ended a level
+
+    pass
+
+
+if __name__ == '__main__':
+    tree_root = BinaryTree.single_line_input(input_array(""))
+    BinaryTree.display(tree_root)
+    left_view_of_binary_tree(tree_root)
+
+"""
+1 2 3 4 5 -1 6 7 -1 -1 8 -1 -1 -1 -1 -1 -1
+"""
